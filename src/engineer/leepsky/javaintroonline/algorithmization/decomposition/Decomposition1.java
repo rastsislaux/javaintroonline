e9a3fba3784241a534f855084f580c88357ea279
ungaf <ungaf@ya.ru>
2021-11-27 21:11:39 +0300

    algorithmization/decomposition 1

---
+++ src/engineer/leepsky/javaintroonline/algorithmization/decomposition/Decomposition1.java
@@ -0,0 +1,19 @@
+package engineer.leepsky.javaintroonline.algorithmization.decomposition;
+
+public class Decomposition1 {
+
+    /* Написать метод(методы) для нахождения наибольшего общего делителя и наименьшего общего кратного двух
+       натуральных чисел: НОК(A, B) = (A*B / НОД(A,B)) */
+
+       public static int gcd(int a, int b) { return b==0 ? a : gcd(b, a%b); }
+       
+       public static int lcm(int a, int b) { return (a*b)/gcd(a, b); }
+
+       public static void main(String[] args) {
+
+            int a = 6, b = 4;
+            System.out.println("lcm(a, b) = " + lcm(a, b) + ", gcd(a, b) = " + gcd(a,b));
+
+       }
+
+}
