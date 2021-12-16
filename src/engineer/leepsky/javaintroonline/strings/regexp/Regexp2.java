71269272ee2902de4916f99acae9502e37805a99
ungaf <ungaf@ya.ru>
2021-12-16 23:31:33 +0300

    strings/regexp 2 fix

--- src/engineer/leepsky/javaintroonline/strings/regexp/Regexp2.java
+++ src/engineer/leepsky/javaintroonline/strings/regexp/Regexp2.java
@@ -12,33 +12,23 @@ public class Regexp2 {
     public static String analyzeXML(String xml) {
 
         Pattern fullTag = Pattern.compile("<([^\\ /<>]+)([^<>]*)>(.*?)<(\\/\\1)>");
-
         Matcher fullTagMatcher = fullTag.matcher(xml);
 
         StringBuilder result = new StringBuilder();
 
         while ( fullTagMatcher.find() ) {
 
-            result.append("Tag:\n\t<");
-            result.append(fullTagMatcher.group(1));
-            result.append(fullTagMatcher.group(2));
-            result.append(">");
-            result.append(fullTagMatcher.group(3));
-            result.append("<");
-            result.append(fullTagMatcher.group(4));
-            result.append(">");
-            result.append("\n\tOpening tag: <");
-            result.append(fullTagMatcher.group(1));
-            result.append(fullTagMatcher.group(2));
-            result.append(">\n\tInner text: ");
-            result.append(fullTagMatcher.group(3));
-            result.append("\n\tClosing tag: <");
-            result.append(fullTagMatcher.group(4));
-            result.append(">\n");
-
-            result.append(
-                analyzeXML(fullTagMatcher.group(3))
-            );
+            result.append("Tag:\n\t<")
+                  .append(fullTagMatcher.group(0))
+                  .append("\n\tOpening tag: <")
+                  .append(fullTagMatcher.group(1))
+                  .append(fullTagMatcher.group(2))
+                  .append(">\n\tInner text: ")
+                  .append(fullTagMatcher.group(3))
+                  .append("\n\tClosing tag: <")
+                  .append(fullTagMatcher.group(4))
+                  .append(">\n")
+                  .append(analyzeXML(fullTagMatcher.group(3)));
 
         }
 
@@ -48,7 +38,8 @@ public class Regexp2 {
 
     public static void main(String[] args) {
 
-        String xml = "<notes><note id = \"1\"><to>Вася</to><from>Света</from><heading>Напоминание</heading><body>Позвони мне завтра!</body></note><note id = \"2\"><to>Петя</to><from>Маша</from><heading>Важное напоминание</heading><body/></note></notes>";
+        String xml = "<notes><note id = \"1\"><to>Вася</to><from>Света</from><heading>Напоминание</heading><body>Позвони мне завтра!</body></note>" +
+                     "<note id = \"2\"><to>Петя</to><from>Маша</from><heading>Важное напоминание</heading><body/></note></notes>";
 
         System.out.println(
             analyzeXML(xml)
